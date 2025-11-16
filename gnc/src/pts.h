4060907d410764e96d17b409cbde3b215830c097
James Li <68095432+jamm-es@users.noreply.github.com>
2025-11-16 03:50:37 +0000

    configurable pts

--- gnc/src/pts.h
+++ gnc/src/pts.h
@@ -25,10 +25,23 @@ struct pt_readings {
     DT_FOREACH_PROP_ELEM(USER_NODE, pt_names, CLOVER_PTS_DT_TO_READINGS_FIELD)
 };
 
+struct pt_config {
+    float scale; // psig per analog reading unit. For teensy, resolution = 12, so for a 1k PT this would be (1000.0 / 4096.0)
+    float bias;
+    float range;
+};
+
+constexpr int NUM_PTS = DT_PROP_LEN(USER_NODE, io_channels);
+extern pt_config pt_configs[NUM_PTS];
+
 int pts_init();
 
 pt_readings pts_sample();
 
 void pts_log_readings(const pt_readings &readings);
+
+int pts_set_bias(int index, float bias);
+
+int pts_set_range(int index, float range);
 
 #endif //CLOVER_PTS_H
